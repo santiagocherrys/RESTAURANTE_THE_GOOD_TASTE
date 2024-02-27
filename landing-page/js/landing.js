266012354e464b9fab6faef674605b85ccf6dbfd
Caro <carito940905@gmail.com>
2024-02-27 15:06:35 -0500

    Enrutamiento desde la landing page hasta la vista galeria y carta

--- landing-page/js/landing.js
+++ landing-page/js/landing.js
@@ -1,7 +1,13 @@
 const logout_btn = document.querySelector('#logout-btn');
+const gallery_menu_btn = document.querySelector('#gallery-menu-btn');
 
 logout_btn.addEventListener('click', ()=>{
     /* Se redirige a login */
     alert("se termina la sesion");
     window.location.href= "/login/login.html";
-})
+});
+
+gallery_menu_btn.addEventListener('click', ()=>{
+    /* Se redirige a galeria y carta */
+    window.location.href= "/gallery-and-cart/index.html";
+});
